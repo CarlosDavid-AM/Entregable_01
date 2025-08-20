8343cd28ed5d27a904818f522a253371c601ebfa
Carlos David <apolayamendozaCarlos.com>
2025-08-19 19:33:17 -0500

    Se crea la funcionalidad del Login

--- app/src/main/java/com/example/entregable_e01/Widgets/LoginActivity.java
+++ app/src/main/java/com/example/entregable_e01/Widgets/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.entregable_e01.Widgets;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.entregable_e01.MainActivity;
 import com.example.entregable_e01.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername, edtPassword;
+    Button btnLogin;
+
+    private void loadUI() {
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    String user = "admin";
+    String pass = "Password123";
+
+    public void Login() {
+        if (edtUsername.getText().toString().equals(user) && edtPassword.getText().toString().equals(pass)) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +46,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        loadUI();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
         });
     }
 }
